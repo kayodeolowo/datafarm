c8e76d71c1fe670272b195f6e8ddea80c6cb6e15
kayodeolowo <officialkayodeolowo@gmail.com>
2023-08-01 04:40:35 +0100

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is overview');
+  } else if (pathName === '/product') {
+    res.end('This is product');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page not found');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Server is running');
+});
